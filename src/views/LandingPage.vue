3817fa878e8c259f9a08b9eacca728dc01bce853
Bayik4 <ayikarbail@gmail.com>
2024-01-19 22:00:41 +0700

    mengubah file landing page dan menambah file login page

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,51 +1,91 @@
 <template>
-    <ion-page>
-        <ion-content>
-            <div class="container">
-                <ion-item>
-                    <ion-input label="Username" label-placement="floating" placeholder="Enter Username"></ion-input>
-                </ion-item>
-                <ion-item>
-                    <ion-input label="Password" type="password" label-placement="floating" placeholder="Enter Username"></ion-input>
-                </ion-item>
-                <ion-item class="btn">
-                    <ion-button class="btn-login" shape="round">Login</ion-button>
-                    <ion-button class="btn-reg" shape="round">Register</ion-button>
-                </ion-item>
-            </div>
-        </ion-content>
-    </ion-page>
+  <ion-page>
+    <ion-content>
+      <div class="container">
+        <h1>WELCOME</h1>
+        <div class="btn">
+          <ion-button class="lp-btn" href="/login">Sign In</ion-button>
+          <ion-button class="lp-btn" href="/register">Sign Up</ion-button>
+        </div>
+      </div>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage,
-    IonHeader, 
-    IonToolbar, 
-    IonTitle, 
-    IonContent, 
-    IonInput, 
-    IonItem,
-    IonButton} from '@ionic/vue';
+import { IonPage, IonContent, IonButton } from "@ionic/vue";
 </script>
 
 <style>
-    .container {
-        border: 1px solid black;
-        width: 300px;
-        margin: 100px auto;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Courier New", Courier, monospace;
+}
 
-    ion-button {
-        width: 100px;
-    }
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 
-    .btn {
-        border: 1px solid black;    
-    }
-    
+.container h1 {
+  text-align: center;
+  font-size: 60px;
+  font-weight: bold;
+  margin-bottom: 70px;
+}
+
+.btn {
+  display: flex;
+  flex-direction: column;
+}
+
+.lp-btn {
+  --background: rgb(230, 230, 230);
+  --background-hover: rgb(220, 220, 220);
+  --background-activated: rgb(215, 215, 215);
+  --background-focused: rgb(220, 220, 220);
+
+  --color: black;
+
+  --border-radius: 5px;
+  /* --border-color: #000;
+  --border-style: solid;
+  --border-width: 0px; */
+
+  /* --box-shadow: 0 2px 6px 0 rgb(0, 0, 0, 0.25); */
+
+  --ripple-color: rgb(100, 100, 100);
+
+  --padding-top: 10px;
+  --padding-bottom: 10px;
+  margin: 5px;
+  width: 160px;
+}
+
+.lp-btn:nth-child(2) {
+  --background: transparent;
+  --background-hover: rgb(220, 220, 220);
+  --background-activated: rgb(215, 215, 215);
+  --background-focused: rgb(220, 220, 220);
+
+  --color: black;
+
+  --border-radius: 5px;
+  --border-color: #000;
+  --border-style: solid;
+  --border-width: 1px;
+
+  /* --box-shadow: 0 2px 6px 0 rgb(0, 0, 0, 0.25); */
+
+  --ripple-color: rgb(100, 100, 100);
+
+  --padding-top: 10px;
+  --padding-bottom: 10px;
+  margin: 5px;
+  width: 160px;
+}
 </style>
-
---
+++ src/views/auth/LoginPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <div class="con-login">
+        <h1>Login</h1>
+        <div class="input">
+          <ion-item class="user">
+            <ion-input label="Username" label-placement="floating" placeholder="Enter Username" class="input-login" :maxlength="20"></ion-input>
+          </ion-item>
+          <ion-item class="pass">
+            <ion-input type="password" label="Password" label-placement="floating" placeholder="Enter Password" class="input-login" :maxlength="20"></ion-input>
+          </ion-item>
+          <ion-button class="login">Login</ion-button>
+        </div>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonContent, IonInput, IonButton, IonItem } from "@ionic/vue";
+</script>
+
+<style>
+ion-input.input-login {
+  --background: transparent;
+  --color: black;
+  --placeholder-color: black;
+  --placeholder-opacity: 0.8;
+  --highlight-color-focused: black;
+
+  --padding-bottom: 10px;
+  --padding-top: 10px;
+  --padding-end: 10px;
+  --padding-start: 10px;
+}
+
+ion-item.user {
+  margin-bottom: 20px;
+}
+</style>
